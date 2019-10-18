0ca2fccb9d53173e65de91aa99a35b780b4d5f96
thanigaivel795 <43550073+thanigaivel795@users.noreply.github.com>
2019-10-19 03:01:38 +0530

    xml

--- JenkinsConfiguration/src/main/java/one/First.java
+++ JenkinsConfiguration/src/main/java/one/First.java
@@ -2,11 +2,14 @@ package one;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class First {
-	public static void main(String[] args) {
+	
+	@Test
+	public static void main() {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 	}
